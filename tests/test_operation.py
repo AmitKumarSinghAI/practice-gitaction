6cb274b2a2038564f2f35bec3bf8903c5cdf772c
AmitKumarSinghAI <amitkumarsinghkurmi123@gmail.com>
2026-08-18 14:18:17 +0530

    inital chage

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -2,7 +2,7 @@ from src.math_operation import mul
 
 def test_mul():
     assert mul(2,3) == 6
-    assert mul(2,50) == 50
+    assert mul(2,25) == 50
     assert mul(2,2) == 4
     assert mul(2,1) == 2
     assert mul(2,0) == 0
